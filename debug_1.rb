0123fb134de0026ea5650391f73014a7dc5e5e90
tsyan <anna.tsykalova@gmail.com>
2015-01-12 15:42:54 -0500

    updated

---
+++ debug_1.rb
@@ -0,0 +1,6 @@
+my_favorite_color = "purple"
+your_favorite_color = "red"
+
+def rainbow(my_favorite_color)
+  "How come #{your_favorite_color} is in ROY G BIV but #{my_favorite_color} isn't?"
+end
---
+++ debug_2.rb
@@ -0,0 +1,6 @@
+movies = ["Fight Club", "Dark Knight", "The Hobbit"]
+
+def best_movie_of_the_90s(movies)
+  movies["Fight Club"]
+end
+
